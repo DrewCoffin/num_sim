637ada1d57d70daac700c5d27819ca1cfeda07af
Drew Coffin <dacoffin@alaska.edu>
2017-01-25 21:00:54 -0900

    Add files via upload
    Newton cooling script

---
+++ Newton.py
@@ -0,0 +1,42 @@
+#Solving Newton Cooling via Euler's Method
+#Drew Coffin
+#January 25, 2017
+#Python 3.5
+
+#Necessary modules
+import matplotlib.pyplot as plt
+import numpy as np
+
+#The data set we wish to match
+def GetArrays():
+    times = list(range(16)) #0,1,2...15 array. Note Python starts at 0
+    Temps = [83, 77.7, 75.1, 73, 71.1, 69.4, 67.8, 66.4, 64.7, 63.4, 62.1, 61, 59.9, 58.7, 57.8, 56.6]
+    return times, Temps
+  
+#Various constants  
+def constants(times, Temps):
+    T0 = Temps[0] #initial temperature
+    tf = times[-1] #Gets final time value
+    Tair = 46 #ambient temperature
+    R = 0.08 #rate of cooling
+    return T0, tf, Tair, R
+
+def Euler(tf, T0, R, nsteps):
+    deltime = tf/nsteps #length of a time step
+    newtimes = np.arange(0,15,deltime) #Generates n time points
+    newTemps = []
+    newTemps.append(T0) #Same start point
+    for i in range(nsteps-1):
+        nextTemp = newTemps[i] - R*(newTemps[i] -Tair)*deltime
+        newTemps.append(nextTemp) #Add new temperature to Euler array
+#    print(len(newtimes), len(newTemps))
+    return newtimes, newTemps
+
+#Main program
+times, Temps = GetArrays()
+T0, tf, Tair, R = constants(times, Temps)
+Eulertimes, EulerTemps = Euler(tf, T0, R, 100) #The Euler method
+plt.plot(times, Temps, Eulertimes, EulerTemps) #Make the plot
+plt.xlabel('time (min)')
+plt.ylabel('Temp (C)')
+plt.show()    
