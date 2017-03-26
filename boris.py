0127ccf87f88bad79138bd43461fa6c7cf84b70a
Drew Coffin <dacoffin@alaska.edu>
2017-03-26 12:54:23 -0800

    Add files via upload

---
+++ boris.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 25 21:16:45 2017
+
+@author: Drew Coffin
+"""
+
+'''Note the electric field is q*E/m, which is units of acceleration.
+The magnetic field is q*B/m, which is units of (gyro)frequency.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def vplus(vold,Bold,delT):
+    Bmag = np.dot(Bold,Bold)**(0.5)
+    alpha = (1 - Bmag*delT**2/4)*vold
+    beta = delT*np.cross(vold,Bold)
+    gamma = 0.5*delT**2*np.dot(vold,Bold)*Bold
+    denom = 1 + Bmag*delT**2/4
+    return np.array((alpha+beta+gamma)/denom)
+    
+def vnew(vplus, delT, Eold):
+    return np.array(vplus + delT*Eold)
+
+B0 = np.array([0,0,1])
+E0 = np.array([0,0.1,0])
+v0 = np.array([1,0,0])
+Tgyro = 2*np.pi/B0[2]
+Tfin = 10*Tgyro
+delT = Tgyro*0.01
+ntsteps = int(Tfin/delT)
+
+pos0 = np.array([0,1,0])
+
+i = 0
+pos = [float(pos0[i]) for i in range(len(pos0))] #Make sure initial position is in floats
+xarr = [pos[0]]
+yarr = [pos[1]]
+vel = v0
+while i < ntsteps:
+    vp = vplus(vel,B0,delT)
+    vn = vnew(vp,delT,E0)
+    pos += delT*vn
+    vel = vn #Update the velocity
+    xarr.append(pos[0])
+    yarr.append(pos[1])
+    #if i/1000 - int(i/1000) == 0:
+     #   print(i) #tracker
+    i += 1
+
+plt.plot(xarr, yarr, 'b')
+plt.axis('equal')
+#plt.axis([-1.8,1.8,-1.2,1.2])
+plt.show()
